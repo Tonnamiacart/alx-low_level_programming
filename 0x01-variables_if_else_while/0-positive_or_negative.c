ef908580c4a179b837891d64595e8f39b0c430dd
Tonnamiacart <tonnamiacart@gmail.com>
2023-09-08 14:53:51 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-/*
- *  main - Print for +ve, _ve and zero
+/**
+ * main - Print for +ve, _ve and zero
  *
  * Return: Always 0 (success)
  */

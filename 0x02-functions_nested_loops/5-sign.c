839556ae5fcde8b3045bc979327d1f0ef84a102d
Tonnamiacart <tonnamiacart@gmail.com>
2023-09-12 19:06:27 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - This prints the sign of a number
+ *
+ * @n: The number to be checked
+ *
+ * Return: 1 for +ve number, -1 for -ve number or 0 for anything else
+ */
+
+int print_sign(int n)
+
+{
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n < 0)
+{
+_putchar(45);
+return (-1);
+}
+else
+{
+_putchar(48);
+return (0);
+}
+
+}

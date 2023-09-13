469ba764df43efbb92a93c649909a643ce9a9e72
Tonnamiacart <tonnamiacart@gmail.com>
2023-09-12 17:54:10 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Making the alphabet for my task
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+char  ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}

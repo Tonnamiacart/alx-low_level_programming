f951b23edd48be3195a08186f4a2d465998dfe2a
Tonnamiacart <tonnamiacart@gmail.com>
2023-09-14 12:37:22 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - This is to check the multiple of the integer
+ * @a: To access the parameter 1
+ * @b: To access the parameter 2
+ * Return: 0;
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

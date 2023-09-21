4ee87f3a1083096bb41c81f5bced033cb0a8592d
Tonnamiacart <tonnamiacart@gmail.com>
2023-09-21 04:41:04 -0700

    Cap string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * cap_string - Using the string
+ * @str: Pointer here
+ * Return: str
+ */
+
+char *cap_string(char *str)
+{
+	int capitalizeNext = 1;
+
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+				str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+				str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+				str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		{
+			capitalizeNext = 1;
+		}
+		else if (capitalizeNext)
+		{
+			str[i] = toupper(str[i]);
+			capitalizeNext = 0;
+		}
+		else
+		{
+			str[i] = tolower(str[i]);
+		}
+	}
+	return (str);
+}

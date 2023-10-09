6d9fa189e0e5cb4e17770432454a9dca2bcf0744
Tonnamiacart <tonnamiacart@gmail.com>
2023-10-08 17:14:03 -0700

    Main and Putchar

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_HEADER_H
+#define MAIN_HEADER_H
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+#endif

4cb4feb887fa6d54eca1301f40cea1022865bb37
Tonnamiacart <tonnamiacart@gmail.com>
2023-10-10 17:46:24 -0700

    Task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * init_dog - this prototype initializes the dog's function
+ * @d: the function dog
+ * @name: The name of the dog
+ * @age: The dog's age
+ * @owner: The owner's name
+ */
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}

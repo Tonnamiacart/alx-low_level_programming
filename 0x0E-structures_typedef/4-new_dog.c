8315bdf972578bc3bf8e1610f6db345b2d527871
Tonnamiacart <tonnamiacart@gmail.com>
2023-10-10 18:56:57 -0700

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,20 +12,20 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	int len1, len2;
+	int l1, l2;
 
-	len1 = _strlen(name);
-	len2 = _strlen(owner);
+	l1 = _strlen(name);
+	l2 = _strlen(owner);
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	dog->name = malloc(sizeof(char) * (len1 + 1));
+	dog->name = malloc(sizeof(char) * (l1 + 1));
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	dog->owner = malloc(sizeof(char) * (len2 + 1));
+	dog->owner = malloc(sizeof(char) * (l2 + 1));
 	if (dog->owner == NULL)
 	{
 		free(dog);
@@ -39,41 +39,39 @@ dog_t *new_dog(char *name, float age, char *owner)
 }
 
 /**
- * _strlen - returns the length of a string
+ * _strlen - this function returns the length of a string
  * @s: pointer to string
- *
- * Return: length of string
+ * Return: ola
  */
 int _strlen(char *s)
 {
-	int len = 0;
+	int ola = 0;
 
 	while (*s != '\0')
 	{
-		len++;
+		ola++;
 		s++;
 	}
 
-	return (len);
+	return (ola);
 }
 
 /**
- * _strcpy - copies the string pointed to by src to dest
+ * _strcpy - this copys the string pointed to by src to dest
  * @dest: pointer to destination string
  * @src: pointer to source string
- *
- * Return: pointer to destination string
+ * Return: destination
  */
 char *_strcpy(char *dest, char *src)
 {
-	int len, i;
+	int jide, i;
 
-	len = 0;
-	while (src[len] != '\0')
+	jide = 0;
+	while (src[jide] != '\0')
 	{
-		len++;
+		jide++;
 	}
-	for (i = 0; i < len; i++)
+	for (i = 0; i < jide; i++)
 	{
 		dest[i] = src[i];
 	}

0905339ce87fe498e8e54a39b0fe63f4055feac2
Tonnamiacart <tonnamiacart@gmail.com>
2023-10-10 19:10:51 -0700

    Task Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - main function prints name of the file.
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+Readme is yet to contain details

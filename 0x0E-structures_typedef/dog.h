5a9f437bfcb42bfbe8e25f70849b65ed69e1a722
Tonnamiacart <tonnamiacart@gmail.com>
2023-10-10 17:28:33 -0700

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_HEADER_FILE
 #define MAIN_HEADER_FILE
-
+/**
+ * struct dog - function for dog
+ * @name: This is the dog's name
+ * @age: This is the dog's age
+ * @owner: This is the owner's name
+ */
 struct dog
 {
 	char *name;

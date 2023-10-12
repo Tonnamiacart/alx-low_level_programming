2d0757a7e922fca8c4524156e5df820f0c9c096d
Tonnamiacart <tonnamiacart@gmail.com>
2023-10-11 17:12:12 -0700

    Task 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,8 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+#include <stddef.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
